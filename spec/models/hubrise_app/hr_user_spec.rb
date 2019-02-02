1d812e20cf15ea50e431b12acf38a5b452c0190d
nsave <nick.saveljev@gmail.com>
2019-02-02 17:26:18 +0300

    Add spec for user app_instances expiration

--- spec/models/hubrise_app/hr_user_spec.rb
+++ spec/models/hubrise_app/hr_user_spec.rb
@@ -35,4 +35,24 @@ RSpec.describe HubriseApp::HrUser do
       )
     end
   end
+
+  describe "#hr_app_instances" do
+    it "returns only fresh instances" do
+      hr_user = create(:hr_user)
+      hr_app_instances = create_list(:hr_app_instance, 5)
+      {
+        Time.new(2000)            => hr_app_instances[0],
+        Time.new(2001)            => hr_app_instances[1],
+        Time.new(2002, 1, 1)      => hr_app_instances[2],
+        Time.new(2002, 1, 1, 12)  => hr_app_instances[3],
+        Time.new(2002, 1, 2)      => hr_app_instances[4]
+      }.each do |assignment_date, hr_app_instance|
+        Timecop.freeze(assignment_date) { hr_user.assign_hr_app_instance(hr_app_instance) }
+      end
+
+      Timecop.freeze(Time.new(2002, 1, 2, 11)) do
+        expect(hr_user.hr_app_instances.to_a).to eq(hr_app_instances[3..4])
+      end
+    end
+  end
 end
