ebefbddefceb457edaa918ba6788e1984ada6b69
nsave <nick.saveljev@gmail.com>
2020-06-25 13:56:39 +0300

    Update spec factories

--- spec/factories/app_instance_factory.rb
+++ spec/factories/app_instance_factory.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :app_instance, class: AppInstance do
     hr_id { generate_ref }
     access_token { "x_app_access_token" }
+
+    transient do
+      user { nil }
+    end
+
+    after(:create) do |app_instance, evaluator|
+      if evaluator.user
+        evaluator.user.user_app_instances.create!(hr_app_instance_id: app_instance.hr_id, refreshed_at: Time.now)
+      end
+    end
   end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     hr_id { generate_ref }
     refreshed_at { Time.now }
     access_token { with_ref("x_profile_access_token") }
+    locales { "en" }
   end
 end
