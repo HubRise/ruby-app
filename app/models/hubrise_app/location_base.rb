1d287e1bac381a63b1848fd4fdcf33bad7149abd
antmonnier <monniera@gmail.com>
2025-09-01 19:04:15 +0200

    Add address related accessors to Location

--- app/models/hubrise_app/location_base.rb
+++ app/models/hubrise_app/location_base.rb
@@ -3,14 +3,10 @@ module HubriseApp
   class LocationBase < HubriseApp::ApplicationRecord
     self.abstract_class = true
 
-    store_accessor :api_data, :name, :country
+    store_accessor :api_data, :name, :address, :postal_code, :city, :country, :cutoff_time
 
     def timezone
       api_data.dig("timezone", "name")
-    end
-
-    def cutoff_time
-      api_data["cutoff_time"]
     end
   end
 end
